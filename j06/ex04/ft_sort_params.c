a8fdd4300bb783b0f1e752bdb7be4d8dcdd9d579
alex <replay_76230@hotmail.fr>
2019-02-22 13:38:03 +0100

    d06ex04

--- j06/ex04/ft_sort_params.c
+++ j06/ex04/ft_sort_params.c
@@ -1,36 +1,51 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-void ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
 
 void	ft_putstr(char *str)
 {
-	while (*str)
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
 	{
-		ft_putchar(*str++);
+		ft_putchar(str[i]);
+		i++;
 	}
 }
 
-int main(int argc, char **argv)
+int		ft_strcmp(char *s1, char *s2)
 {
-	{
-		char *temp; 
-		int i;
-		int j;
-		while (i == 0 && i < argc)
+	int i;
+
+	i = 0;
+	while ((s1[i] == s2[i]) && (s1[i] != '\0' && s2[i] != '\0'))
 		i++;
-			while (j == 0 && j < argc)
-			j++;
-				if (strcmp(argv[i], argv[j]) < 0)
-				{
-					temp = argv[i];
-					argv[i] = argv[j];
-					argv[j] = temp;
-				}
+	return (s1[i] - s2[i]);
+}
+
+int		main(int argc, char **argv)
+{
+	char	*tmp;
+	int		find;
+	int		i;
+
+	find = 1;
+	while (find)
+	{
+		find = 0;
+		i = 0;
+		while (++i < argc - 1)
+		{
+			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+			{
+				tmp = argv[i];
+				argv[i] = argv[i + 1];
+				argv[i + 1] = tmp;
+				find = 1;
+			}
+		}
 	}
-    return (0);
+	i = 0;
+	while (++i < argc)
+		ft_putstr(argv[i]), ft_putchar('\n');
+	return (0);
 }
