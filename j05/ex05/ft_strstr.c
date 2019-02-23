4137dbe7ec949f855a5bc6da21a02f84e3b88d3a
replay76 <47487590+replay76@users.noreply.github.com>
2019-02-23 22:39:42 +0100

    update

--- j05/ex05/ft_strstr.c
+++ j05/ex05/ft_strstr.c
@@ -1,32 +1,23 @@
 #include <string.h>
-#include <stdio.h>
 
-char *ft_strstr(char *str, char *to_find)
+char    *ft_strstr(char *str, char *to_find)
 {
-    long i;
-    long j;
+    int i;
+    int j;
 
     i = 0;
-    j = 0;
-    while(str[i]!='\0')
-    {
-    	printf("condition 1 \n");
-        while(str[i + j] == to_find[j] && str[i + j])
-        {
-    	printf("condition 2 \n");
-            i++;
-        }
-        if(to_find[j] != '\0')
-        {              
-    	printf("condition 3 \n");
-        	return (str + i);
-    	}
-    j++;
-    }
-    if(str[0] =='\0' && to_find[0] =='\0')
-    {
-    	printf("condition 4 \n");
-    	return (str);
-    }
-return ((void*) 0);
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (to_find[j] == str[i + j])
+		{
+			if (to_find[j + 1] == '\0')
+			{
+				return (str + i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
